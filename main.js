fc7fffaa14a662ad7578e0282c7027ec651b7146
Nicolas Carvalho <nickcarva31@gmail.com>
2025-05-30 20:28:57 -0300

    Verificação do preenchimento da input item

--- main.js
+++ main.js
@@ -2,7 +2,13 @@ let items = []
 
 function addItem() {
     const itemName = document.querySelector("#item").value
+
+    if (itemName === "") {
+        alert("Digite um item para adicionar")
+        return
+    }
     
+
     const item = {
         name: itemName,
         checked: false
